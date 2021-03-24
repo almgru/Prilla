70ed7acf9f4dde9b1ddf0fb8d1dcf6b543f3091f
Daniel Alm Grundström <daniel.alm.grundstrom@protonmail.com>
2021-03-24 09:14:08 +0100

    Project setup

---
+++ src/main/java/com/almgru/trabacco/TrabaccoApplication.java
@@ -0,0 +1,14 @@
+package com.almgru.trabacco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabaccoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrabaccoApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/almgru/trabacco/TrabaccoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.almgru.trabacco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabaccoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
