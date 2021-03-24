65a795805cd130bb5de25ebe7c2e9110d54f10a2
Daniel Alm Grundström <daniel.alm.grundstrom@protonmail.com>
2021-03-24 13:43:37 +0100

    Optimizes imports

--- src/main/java/com/almgru/trabacco/controller/Record.java
+++ src/main/java/com/almgru/trabacco/controller/Record.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.LocalDate;
-import java.util.Map;
 
 @Controller
 public class Record {
