83fd05771bd9aff9d553dd3cb91165bc4ac37a4b
Daniel Alm Grundström <daniel.alm.grundstrom@protonmail.com>
2021-04-07 15:51:04 +0200

    Moves chart to separate page

---
+++ src/main/java/com/almgru/trabacco/controller/ChartsController.java
@@ -0,0 +1,12 @@
+package com.almgru.trabacco.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ChartsController {
+    @GetMapping("/charts")
+    public String charts() {
+        return "charts";
+    }
+}
