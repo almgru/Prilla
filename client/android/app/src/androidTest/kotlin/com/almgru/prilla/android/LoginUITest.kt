b960471b20d62574f5445228de2ff13977908ecf
Daniel Alm Grundström <almgru+github@fastmail.com>
2023-08-13 03:22:25 +0200

    Restore test timeout

--- client/android/app/src/androidTest/kotlin/com/almgru/prilla/android/LoginUITest.kt
+++ client/android/app/src/androidTest/kotlin/com/almgru/prilla/android/LoginUITest.kt
@@ -47,7 +47,7 @@ class LoginUITest {
 
     @Test
     fun login_press_with_correct_inputs_shows_main_view() {
-        val loginTimeoutMs = 10_000L
+        val loginTimeoutMs = 500L
 
         mockServer.enqueue(
             MockResponse()
